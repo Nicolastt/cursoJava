79ad5b60060a0d6186bc96870b589be377a8c94c
Nicholas <gianfrancopr08@gmail.com>
2023-04-17 18:04:48 -0500

    Se realizó el ejercicio 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenido al curso!");
+        System.out.println("Ejercicio 4");
+
+        int contador = 0;
+        while (contador <= 10){
+            System.out.println(contador);
+            contador++;
+        }
+
+        for (int i = 0; i <= 10; i++){
+            System.out.println(i);
+        }
+
+        do {
+            System.out.println(contador);
+        }while (contador <= 10);
+
+
     }
 }
