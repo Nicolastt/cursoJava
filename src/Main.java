f168019fca8d00e74d9ce236b7b4706614c01370
Nicholas <gianfrancopr08@gmail.com>
2023-04-17 16:38:04 -0500

    Se ha escrito el ejercicio 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenido al curso!");
+        System.out.println("Ejercicio3");
+        int mes = 1221;
+
+        switch (mes) {
+            case 1 -> System.out.println("ENERO");
+            case 2 -> System.out.println("FEBRERO");
+            case 3 -> System.out.println("MARZO");
+            case 4 -> System.out.println("ABRIL");
+            case 5 -> System.out.println("MAYO");
+            case 6 -> System.out.println("El mes es junio");
+            case 7 -> System.out.println("El mes es julio");
+            case 8 -> System.out.println("El mes es agosto");
+            case 9 -> System.out.println("El mes es septiembre");
+            case 10 -> System.out.println("El mes es octubre");
+            case 11 -> System.out.println("El mes es noviembre");
+            case 12 -> System.out.println("El mes es diciembre");
+            default -> System.out.println("Mes inválido mi perrito");
+        }
+
+        //System.out.println("Hola");
     }
 }
