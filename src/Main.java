c582b3a7059cd969c966547e1ea726fcade46861
Nicholas <gianfrancopr08@gmail.com>
2023-04-17 17:45:30 -0500

    Se utilizo unos JOptionPane para darle mas vida al programa

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,37 @@
+import javax.swing.*;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ejercicio N° 1");
-        int edad = 18;
-        boolean vieneConUnAdulto = true;
-        boolean esMayorDeEdad = edad >= 18; //Es mayor de edad si tiene más de 18 años
 
-        if (esMayorDeEdad || vieneConUnAdulto){
+        int edad1;
+        boolean vieneConUnAdulto;
+
+        do {
+            edad1 = Integer.parseInt(pedirEdad());
+        } while (edad1 < 0 || edad1 > 30);
+
+        vieneConUnAdulto = Objects.equals(preguntarAcompanante(), "S");
+
+        boolean esMayorDeEdad = edad1 >= 18; //Es mayor de edad si tiene más de 18 años
+
+        if (esMayorDeEdad || vieneConUnAdulto) {
             System.out.println("Puede entrar");
         } else {
             System.out.println("Fuera de aquí");
         }
     }
+
+    private static String preguntarAcompanante() {
+        String tieneAcompanate = JOptionPane.showInputDialog("Escribe S si vienes acompañado con un adulto");
+        return tieneAcompanate;
+    }
+
+    private static String pedirEdad() {
+        String edad = JOptionPane.showInputDialog("Introduce tu edad");
+        return edad;
+    }
+
+
 }
