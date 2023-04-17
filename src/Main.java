5480d169c6151fd06e43de9a68a7d6ddd4f24d6a
Nicholas <gianfrancopr08@gmail.com>
2023-04-16 23:02:46 -0500

    Se ha escrito el ejercicio 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ejercicio N° 1");
+        int edad = 18;
+        boolean vieneConUnAdulto = true;
+        boolean esMayorDeEdad = edad >= 18; //Es mayor de edad si tiene más de 18 años
+
+        if (esMayorDeEdad || vieneConUnAdulto){
+            System.out.println("Puede entrar");
+        } else {
+            System.out.println("Fuera de aquí");
+        }
     }
 }
