ad2055c98df220e47978e34f8ada1e4dae950775
Nicholas <gianfrancopr08@gmail.com>
2023-04-17 18:35:33 -0500

    Se realizó la serie de Fibonacci con ciclos for y un array

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ejercicio 4");
 
-        int contador = 0;
-        int suma = 0;
-        while (contador <= 10){
-            System.out.println(contador);
-            suma += contador;
-            contador++;
+        int nTerminos = 10;
+        int[] fibo = new int[nTerminos];
+
+        for (int i = 0; i < fibo.length; i++) {
+            if (i == 0) {
+                fibo[i] = 0;
+            } else if (i == 1) {
+                fibo[i] = 1;
+            } else {
+                fibo[i] = fibo[i - 1] + fibo[i - 2];
+            }
         }
 
-        System.out.println("La suma de todos los números es: " + suma);
+        for (int n : fibo) {
+            System.out.println(n + " ");
+        }
 
     }
 }
