7bffbda57220d0293b7b5ba3c7f39dd187475c74
Nicholas <gianfrancopr08@gmail.com>
2023-04-17 21:42:27 -0500

    Se implementó un for que calcula el factorial.

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ejercicio 9");
 
-        System.out.println(calcularFactorial(5));
-
-    }
-
-    private static int calcularFactorial(int n) {
-
-        int resultado = 0;
-
-        for (int i = 1; i <= n; i++) {
-            if (n == 1) {
-                resultado = 1;
-            } else {
-                resultado = n * calcularFactorial(n-1);
-            }
+        int factorial = 1;
+        for (int i = 1; i < 11; i++) {
+            factorial *= i;
+            System.out.println("Factorial de " + i + " = " + factorial);
         }
-        return resultado;
+
     }
 }
