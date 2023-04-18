f22628833fa29fb199c335659a4305f8b269209f
Nicholas <gianfrancopr08@gmail.com>
2023-04-17 20:43:55 -0500

    Se realizó media pirámide con números, en donde se va incrementando.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenido al curso!");
+        System.out.println("Ejercicio 7");
+
+        //1
+        //12
+        //123
+        //1234
+        //12345
+
+        int n = 6;
+
+        for (int fila = 1; fila <= n; fila++) {
+            for (int col = 1; col < fila; col++) {
+                System.out.print(col);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
