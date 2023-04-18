52312031d5c371f78f62840f45cdd676e8bc57fc
Nicholas <gianfrancopr08@gmail.com>
2023-04-17 20:23:56 -0500

    Se realizó las tablas de multiplicar con ciclos.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenido al curso!");
+        System.out.println("Ejercicio 5");
+        for (int fila = 1 ; fila <= 10; fila++){
+            for (int columna = 1; columna <= 10; columna++){
+                System.out.print(fila*columna);
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
     }
 }
