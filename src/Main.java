b3a44da7829865fed6e6fd543cc22c163b6ef0de
Nicholas <gianfrancopr08@gmail.com>
2023-04-17 21:40:27 -0500

    Se implementó un for que calcula el factorial con recursividad.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenido al curso!");
+        System.out.println("Ejercicio 9");
+
+        System.out.println(calcularFactorial(5));
+
+    }
+
+    private static int calcularFactorial(int n) {
+
+        int resultado = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n == 1) {
+                resultado = 1;
+            } else {
+                resultado = n * calcularFactorial(n-1);
+            }
+        }
+        return resultado;
     }
 }
