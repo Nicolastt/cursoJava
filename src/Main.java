f9489eead96d78f664eddcc24ace5f16f7c9ea81
Nicholas <gianfrancopr08@gmail.com>
2023-04-17 20:51:09 -0500

    Se implementó un for que imprime todos los múltiplos de 3, entre 1 y 100.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Ejercicio 8");
+        //Imprimir todos los múltiplos de 3, entre 1 y 100.
+        for (int numero = 0; numero <= 100; numero++){
+            if (numero%3 == 0){
+                System.out.println(numero);
+            }
+        }
     }
 }
