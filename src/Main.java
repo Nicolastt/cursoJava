4b4e261190dab87411e08c375a0b019f0ad11afa
Nicholas <gianfrancopr08@gmail.com>
2023-04-17 20:34:41 -0500

    Se realizó media pirámide con el break.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenido al curso!");
+        System.out.println("Ejercicio 6");
+
+        int n = 5;
+
+        for (int fila = 0; fila <= n; fila++) {
+            for (int col = 0; col <= n; col++) {
+                if (col > fila) {
+                    break;
+                }
+
+                System.out.print("*");
+                System.out.print(" ");
+
+            }
+            System.out.println();
+        }
     }
 }
